99b2240ed700d5c5dbe6df8512ff6104762dc3c6
Eduard Bazhneov <draisor.rid@bk.ru>
2023-05-29 19:26:20 +0300

    source

---
+++ src/pass_lib/password.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+
+
+int random_int(int min, int max);
+char random_symbol(char *symbols, int num_symbols);
+void generate_password(char *password, int length, int use_uppercase, int use_numbers, int use_special);
+
+
+
