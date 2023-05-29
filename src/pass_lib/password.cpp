59d5018651a743c3e694955e8996ff823e36be6b
Eduard Bazhneov <draisor.rid@bk.ru>
2023-05-29 20:59:12 +0300

    Update files

--- src/pass_lib/password.cpp
+++ src/pass_lib/password.cpp
@@ -1,35 +1,40 @@
 
 #include <pass_lib/password.h>
 
-// функция, генерирующая случайное целое число в заданном диапазоне
+
 int random_int(int min, int max) {
     return rand() % (max - min + 1) + min;
 }
 
-// функция, генерирующая случайный символ из заданного набора
+
 char random_symbol(char *symbols, int num_symbols) {
     int index = rand() % num_symbols;
     return symbols[index];
 }
 
-// функция, генерирующая пароль заданной длины с заданным набором символов
+
 void generate_password(char *password, int length, int use_uppercase, int use_numbers, int use_special) {
     char symbols[96];
     int num_symbols = 0;
-    for (char c = 'a'; c <= 'z'; c++) {
+    for (char c = 'a'; c <= 'z'; c++) 
+    {
         symbols[num_symbols++] = c;
     }
-    if (use_uppercase) {
+    if (use_uppercase) 
+    {
         for (char c = 'A'; c <= 'Z'; c++) {
             symbols[num_symbols++] = c;
         }
     }
-    if (use_numbers) {
+    if (use_numbers) 
+    {
         for (char c = '0'; c <= '9'; c++) {
             symbols[num_symbols++] = c;
         }
     }
-    if (use_special) {
+    
+    if (use_special) 
+    {
         char special_symbols[] = "!@#$%^&*()_+-=";
         for (int i = 0; i < sizeof(special_symbols) - 1; i++) {
             symbols[num_symbols++] = special_symbols[i];
@@ -37,7 +42,8 @@ void generate_password(char *password, int length, int use_uppercase, int use_nu
     }
     symbols[num_symbols] = '\0';
 
-    for (int i = 0; i < length; i++) {
+    for (int i = 0; i < length; i++) 
+    {
         password[i] = random_symbol(symbols, num_symbols);
     }
     password[length] = '\0';
