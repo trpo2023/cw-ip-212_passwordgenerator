d4bdbfeb0f058ec3bb03b9411698750c8a8e7e46
root <root@DESKTOP-1DRSQDI.localdomain>
2023-05-30 19:15:32 +0300

    gitignore

--- test/game_test.c
+++ test/game_test.c
@@ -1,10 +1,2 @@
 #include <ctest.h>
 #include <pass_lib/password.h>
-
-CTEST(gen_random_int, correct_input)
-{
-    int min = 2;
-    int max = 55;
-    int result = random_int(int min, int max);
-    ASSERT_INTERVAL(min, max, result);
-}
